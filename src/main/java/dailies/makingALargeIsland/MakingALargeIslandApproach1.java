73785fead72cc842a1f82ff8bde52b580ec5f900
stancial <alexander.stanciu@microfocus.com>
2021-08-01 20:35:34 +0300

    improved speed by only checking border zeros, labeling islands and keeping a map of island sizes

--- src/main/java/dailies/makingALargeIsland/MakingALargeIslandApproach1.java
+++ src/main/java/dailies/makingALargeIsland/MakingALargeIslandApproach1.java
@@ -9,13 +9,17 @@ public class MakingALargeIslandApproach1 implements MakingALargeIsland {
     @Override
     public int largestIsland(int[][] grid) {
         List<Island> islands = new ArrayList<>();
+        Map<Integer, Integer> sizeByLabel = new HashMap<>();
         int n = grid.length;
         List<MatrixIndex> listOfZeros = new ArrayList<>();
 
+        int currentLabel = 2;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    islands.add(new Island(grid, new MatrixIndex(i, j)));
+                    Island island = new Island(grid, new MatrixIndex(i, j), currentLabel);
+                    islands.add(island);
+                    sizeByLabel.put(currentLabel++, island.ones.size());
                 }
                 if (grid[i][j] == 0) {
                     listOfZeros.add(new MatrixIndex(i, j));
@@ -31,21 +35,10 @@ public class MakingALargeIslandApproach1 implements MakingALargeIsland {
         }
         int max = 1;
 
-        for (MatrixIndex zeroIndex : listOfZeros) {
-            int maxChangeForThisZero = 1;
-            List<MatrixIndex> zeroNeighbours = zeroIndex.getCardinalNeighbours(n);
-            Set<Island> neighbouringIslands = new HashSet<>();
-            for (MatrixIndex zeroNeighbour : zeroNeighbours) {
-                if (grid[zeroNeighbour.i][zeroNeighbour.j] != 0) {
-                    Island neighbourIsland = islands.stream().filter(island -> island.ones.contains(zeroNeighbour)).findFirst().orElseThrow();
-                    neighbouringIslands.add(neighbourIsland);
-                }
-            }
-            for (Island neighbour : neighbouringIslands) {
-                maxChangeForThisZero += neighbour.ones.size();
-            }
-            if (max < maxChangeForThisZero) {
-                max = maxChangeForThisZero;
+        for (Island island : islands) {
+            int tempMax = island.potentialMaxSize(sizeByLabel, grid);
+            if (max < tempMax) {
+                max = tempMax;
             }
         }
 
@@ -54,20 +47,41 @@ public class MakingALargeIslandApproach1 implements MakingALargeIsland {
 
     static class Island {
         List<MatrixIndex> ones;
+        List<MatrixIndex> border;
 
-        Island(int[][] grid, MatrixIndex start) {
+        Island(int[][] grid, MatrixIndex start, int label) {
             ones = new ArrayList<>();
+            border = new ArrayList<>();
             int n = grid.length;
             Queue<MatrixIndex> queue = new LinkedList<>();
             queue.add(start);
             while (!queue.isEmpty()) {
                 MatrixIndex currentIndex = queue.poll();
                 if (grid[currentIndex.i][currentIndex.j] == 1) {
-                    grid[currentIndex.i][currentIndex.j] = -1;
+                    grid[currentIndex.i][currentIndex.j] = label;
                     ones.add(currentIndex);
                     queue.addAll(currentIndex.getCardinalNeighbours(n));
                 }
+                if (grid[currentIndex.i][currentIndex.j] == 0) {
+                    border.add(currentIndex);
+                }
             }
+        }
+
+        public int potentialMaxSize(Map<Integer, Integer> sizeByLabel, int[][] grid) {
+            int currentMax = ones.size() + 1;
+            for (MatrixIndex borderElement : border) {
+                Set<Integer> labels = new HashSet<>();
+                for (MatrixIndex neighbour : borderElement.getCardinalNeighbours(grid.length)) {
+                    if (grid[neighbour.i][neighbour.j] > 1)
+                        labels.add(grid[neighbour.i][neighbour.j]);
+                }
+                int tempMax = labels.stream().map(sizeByLabel::get).reduce(1, Integer::sum);
+                if (currentMax < tempMax) {
+                    currentMax = tempMax;
+                }
+            }
+            return currentMax;
         }
     }
 }
