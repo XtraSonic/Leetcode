d36fd854d635ebfc97e880713cb9d5a43c207854
stancial <alexander.stanciu@microfocus.com>
2021-08-11 21:47:04 +0300

    replaced Array lists with hash maps. accepted solution

--- src/main/java/dailies/arrayOfDoubledPairs/ArrayOfDoubledPairsApproach1.java
+++ src/main/java/dailies/arrayOfDoubledPairs/ArrayOfDoubledPairsApproach1.java
@@ -2,49 +2,76 @@ package dailies.arrayOfDoubledPairs;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
 public class ArrayOfDoubledPairsApproach1 implements ArrayOfDoubledPairs {
 
     @Override
     public boolean canReorderDoubled(int[] arr) {
-        List<Integer> oddNumberDoubles = new ArrayList<>();
-        List<Integer> evenNumbers = new ArrayList<>();
+        HashMap<Integer, Integer> oddNumberDoubles = new HashMap<>();
+        HashMap<Integer, Integer> evenNumbers = new HashMap<>();
 
-        for (int currentValue : arr) {
+        for (Integer currentValue : arr) {
             if (currentValue % 2 == 0) {
-                if (oddNumberDoubles.contains(currentValue)) {
-                    oddNumberDoubles.remove(Integer.valueOf(currentValue));
+                if (oddNumberDoubles.containsKey(currentValue)) {
+                    Integer frequency = oddNumberDoubles.get(currentValue);
+                    if (frequency > 1) {
+                        oddNumberDoubles.put(currentValue, frequency - 1);
+                    } else {
+                        oddNumberDoubles.remove(currentValue);
+                    }
                 } else {
-                    evenNumbers.add(currentValue);
+                    if (evenNumbers.containsKey(currentValue)) {
+                        evenNumbers.put(currentValue, evenNumbers.get(currentValue) + 1);
+                    } else {
+                        evenNumbers.put(currentValue, 1);
+                    }
                 }
             } else {
                 int doubleOfTheCurrentValue = currentValue * 2;
-                if (evenNumbers.contains(doubleOfTheCurrentValue)) {
-                    evenNumbers.remove(Integer.valueOf(doubleOfTheCurrentValue));
+                if (evenNumbers.containsKey(doubleOfTheCurrentValue)) {
+                    Integer frequency = evenNumbers.get(doubleOfTheCurrentValue);
+                    if (frequency > 1) {
+                        evenNumbers.put(doubleOfTheCurrentValue, frequency - 1);
+                    } else {
+                        evenNumbers.remove(doubleOfTheCurrentValue);
+                    }
                 } else {
-                    oddNumberDoubles.add(doubleOfTheCurrentValue);
+                    if (oddNumberDoubles.containsKey(doubleOfTheCurrentValue)) {
+                        oddNumberDoubles.put(doubleOfTheCurrentValue, oddNumberDoubles.get(doubleOfTheCurrentValue) + 1);
+                    } else {
+                        oddNumberDoubles.put(doubleOfTheCurrentValue, 1);
+                    }
                 }
             }
         }
 
-        if (!oddNumberDoubles.isEmpty() || evenNumbers.size() % 2 != 0) {
+        if (!oddNumberDoubles.isEmpty()) {
             return false;
         }
 
-        evenNumbers.sort(Comparator.comparingInt(e -> e));
+        List<Integer> sortedIndexes = new ArrayList<>(evenNumbers.keySet());
+        sortedIndexes.sort(Comparator.comparingInt(e -> e));
 
-        while (!evenNumbers.isEmpty()) {
-            Integer currentValue = evenNumbers.get(0);
-            evenNumbers.remove(currentValue);
-            Integer doubleOfTheCurrentNumber;
-            if (currentValue < 0) {
-                doubleOfTheCurrentNumber = currentValue / 2;
+        for (int index : sortedIndexes) {
+            int frequencyOfLowestNumber = evenNumbers.get(index);
+            if (frequencyOfLowestNumber == 0)
+                continue;
+
+            int doubleOfTheCurrentNumber;
+            if (index < 0) {
+                doubleOfTheCurrentNumber = index / 2;
             } else {
-                doubleOfTheCurrentNumber = currentValue * 2;
+                doubleOfTheCurrentNumber = index * 2;
             }
-            if (evenNumbers.contains(doubleOfTheCurrentNumber)) {
-                evenNumbers.remove(doubleOfTheCurrentNumber);
+            if (evenNumbers.containsKey(doubleOfTheCurrentNumber)) {
+                int frequencyOfTheDouble = evenNumbers.get(doubleOfTheCurrentNumber);
+                if (frequencyOfLowestNumber <= frequencyOfTheDouble) {
+                    evenNumbers.put(doubleOfTheCurrentNumber, frequencyOfTheDouble - frequencyOfLowestNumber);
+                } else {
+                    return false;
+                }
             } else {
                 return false;
             }
