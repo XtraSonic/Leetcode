04d9cde4745155aa1f3e12549ef44a23eb443865
stancial <alexander.stanciu@microfocus.com>
2021-08-12 14:30:21 +0300

    improved speed at the cost of making the solution less generic

--- src/main/java/dailies/groupAnagrams/GroupAnagramsApproach1.java
+++ src/main/java/dailies/groupAnagrams/GroupAnagramsApproach1.java
@@ -7,42 +7,21 @@ public class GroupAnagramsApproach1 implements GroupAnagrams {
     @Override
     public List<List<String>> groupAnagrams(String[] strs) {
 
-        Map<CharacterFrequencyCounter, List<String>> characterFrequencyToStringsMap = new HashMap<>();
+        Map<Integer, List<String>> characterFrequencyToStringsMap = new HashMap<>();
 
         for (String string : strs) {
-            CharacterFrequencyCounter cfc = new CharacterFrequencyCounter(string);
-            List<String> listOfStrings = characterFrequencyToStringsMap.getOrDefault(cfc, new ArrayList<>());
+            int[] frequencyMap = new int[26];
+            for (char c : string.toCharArray()) {
+                frequencyMap[c - 'a']++;
+            }
+            int key = Arrays.hashCode(frequencyMap);
+            List<String> listOfStrings = characterFrequencyToStringsMap.getOrDefault(key, new ArrayList<>());
             listOfStrings.add(string);
             if (listOfStrings.size() == 1) {
-                characterFrequencyToStringsMap.put(cfc, listOfStrings);
+                characterFrequencyToStringsMap.put(key, listOfStrings);
             }
         }
 
         return new ArrayList<>(characterFrequencyToStringsMap.values());
-    }
-
-    static class CharacterFrequencyCounter {
-        Map<Character, Integer> frequencyMap;
-
-        public CharacterFrequencyCounter(String s) {
-            frequencyMap = new HashMap<>();
-            for (char c : s.toCharArray()) {
-                int currentFrequency = frequencyMap.getOrDefault(c, 0);
-                frequencyMap.put(c, currentFrequency + 1);
-            }
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            CharacterFrequencyCounter that = (CharacterFrequencyCounter) o;
-            return Objects.equals(frequencyMap, that.frequencyMap);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(frequencyMap);
-        }
     }
 }
