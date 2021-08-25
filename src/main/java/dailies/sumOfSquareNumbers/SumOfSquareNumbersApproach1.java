0d97fb7bc09d44167462600f04bcdfe27809e627
stancial <alexander.stanciu@microfocus.com>
2021-08-25 16:08:03 +0300

    only one pass over squares

--- src/main/java/dailies/sumOfSquareNumbers/SumOfSquareNumbersApproach1.java
+++ src/main/java/dailies/sumOfSquareNumbers/SumOfSquareNumbersApproach1.java
@@ -7,19 +7,12 @@ public class SumOfSquareNumbersApproach1 implements SumOfSquareNumbers {
     @Override
     public boolean judgeSquareSum(int c) {
         int sqrt = (int) Math.floor(Math.sqrt(c));
-        int[] squares = new int[sqrt + 1];
+        HashSet<Integer> squares = new HashSet<>();
         for (int i = 0; i <= sqrt; i++) {
-            squares[i] = i * i;
-        }
-        return canTowSum(squares, c);
-    }
-
-    private boolean canTowSum(int[] squares, int c) {
-        HashSet<Integer> seenNumbers = new HashSet<>();
-        for (int square : squares) {
-            int target = c - square;
-            seenNumbers.add(square);
-            if (seenNumbers.contains(target))
+            int iSquared = i * i;
+            int target = c - iSquared;
+            squares.add(iSquared);
+            if (squares.contains(target))
                 return true;
         }
         return false;
