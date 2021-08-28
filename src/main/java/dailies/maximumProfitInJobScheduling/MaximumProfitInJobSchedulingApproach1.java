ac9a74424414e5ef9b674580e8a98c34462253c2
stancial <alexander.stanciu@microfocus.com>
2021-08-28 16:51:36 +0300

    added sorting after start time

--- src/main/java/dailies/maximumProfitInJobScheduling/MaximumProfitInJobSchedulingApproach1.java
+++ src/main/java/dailies/maximumProfitInJobScheduling/MaximumProfitInJobSchedulingApproach1.java
@@ -1,6 +1,9 @@
 package dailies.maximumProfitInJobScheduling;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 
 public class MaximumProfitInJobSchedulingApproach1 implements MaximumProfitInJobScheduling {
 
@@ -9,26 +12,41 @@ public class MaximumProfitInJobSchedulingApproach1 implements MaximumProfitInJob
     @Override
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
         partialSum = new HashMap<>();
-        int currentTime = 0;
-        int index = 0;
-        return solveJobScheduling(currentTime, index, startTime, endTime, profit);
+        List<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < startTime.length; i++) {
+            jobs.add(new Job(startTime[i], endTime[i], profit[i]));
+        }
+        jobs.sort(Comparator.comparingInt(a -> a.startTime));
+        return solveJobScheduling(0, 0, jobs);
     }
 
-    private int solveJobScheduling(int currentTime, int index, int[] startTime, int[] endTime, int[] profit) {
-        while (index < startTime.length && startTime[index] < currentTime) {
+    private int solveJobScheduling(int currentTime, int index, List<Job> jobs) {
+        while (index < jobs.size() && jobs.get(index).startTime < currentTime) {
             index++;
         }
-        if (index >= startTime.length)
+        if (index >= jobs.size())
             return 0;
-        if (index == startTime.length - 1)
-            return profit[index];
+        if (index == jobs.size() - 1)
+            return jobs.get(index).profit;
         if (partialSum.containsKey(index))
             return partialSum.get(index);
 
-        int assumeNotPickingJob = solveJobScheduling(startTime[index + 1], index + 1, startTime, endTime, profit);
-        int assumePickingJob = solveJobScheduling(endTime[index], index + 1, startTime, endTime, profit);
-        int maxSumForIndex = Math.max(assumeNotPickingJob, assumePickingJob + profit[index]);
+        int assumeNotPickingJob = solveJobScheduling(jobs.get(index + 1).startTime, index + 1, jobs);
+        int assumePickingJob = solveJobScheduling(jobs.get(index).endTime, index + 1, jobs);
+        int maxSumForIndex = Math.max(assumeNotPickingJob, assumePickingJob + jobs.get(index).profit);
         partialSum.put(index, maxSumForIndex);
         return maxSumForIndex;
+    }
+
+    static class Job {
+        int startTime;
+        int endTime;
+        int profit;
+
+        public Job(int startTime, int endTime, int profit) {
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.profit = profit;
+        }
     }
 }
