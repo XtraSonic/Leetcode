0d92ce28df4c36c083bd9fa7f01a183626e2f86a
stancial <alexander.stanciu@microfocus.com>
2021-09-02 20:57:17 +0300

    added equals, hashCode and toString

--- src/main/java/commonUtilities/TreeNode.java
+++ src/main/java/commonUtilities/TreeNode.java
@@ -80,4 +80,22 @@ public class TreeNode {
     public TreeNode getRight() {
         return right;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TreeNode treeNode = (TreeNode) o;
+        return val == treeNode.val && Objects.equals(left, treeNode.left) && Objects.equals(right, treeNode.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, left, right);
+    }
+
+    @Override
+    public String toString() {
+        return toList().toString();
+    }
 }
