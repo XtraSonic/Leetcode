8f09b0f60ce3a9bbe0a3ab1f6abf45651ecf4d1f
stancial <alexander.stanciu@microfocus.com>
2021-09-17 00:19:00 +0300

    solved Spiral Matrix

--- src/main/java/dailies/spiralMatrix/SpiralMatrixApproach1.java
+++ src/main/java/dailies/spiralMatrix/SpiralMatrixApproach1.java
@@ -1,11 +1,64 @@
 package dailies.spiralMatrix;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class SpiralMatrixApproach1 implements SpiralMatrix {
 
     @Override
     public List<Integer> spiralOrder(int[][] matrix) {
-        return null;
+        int minX = 0;
+        int maxX = matrix[0].length - 1;
+        int minY = 0;
+        int maxY = matrix.length - 1;
+
+        int currentDir = 0;
+        List<Integer> result = new LinkedList<>();
+        int currentX = 0;
+        int currentY = 0;
+        result.add(matrix[currentX][currentY]);
+
+        while (minX <= maxX && minY <= maxY) {
+            switch (currentDir) {
+                case 0:
+                    if (currentX + 1 <= maxX) {
+                        currentX++;
+                        result.add(matrix[currentY][currentX]);
+                    } else {
+                        currentDir = (currentDir + 1) % 4;
+                        minY++;
+                    }
+                    break;
+                case 1:
+                    if (currentY + 1 <= maxY) {
+                        currentY++;
+                        result.add(matrix[currentY][currentX]);
+                    } else {
+                        currentDir = (currentDir + 1) % 4;
+                        maxX--;
+                    }
+                    break;
+                case 2:
+                    if (currentX - 1 >= minX) {
+                        currentX--;
+                        result.add(matrix[currentY][currentX]);
+                    } else {
+                        currentDir = (currentDir + 1) % 4;
+                        maxY--;
+                    }
+                    break;
+                case 3:
+                    if (currentY - 1 >= minY) {
+                        currentY--;
+                        result.add(matrix[currentY][currentX]);
+                    } else {
+                        currentDir = (currentDir + 1) % 4;
+                        minX++;
+                    }
+                    break;
+
+            }
+        }
+        return result;
     }
 }
--- src/test/java/dailies/spiralMatrix/SpiralMatrixTest.java
+++ src/test/java/dailies/spiralMatrix/SpiralMatrixTest.java
@@ -1,5 +1,29 @@
 package dailies.spiralMatrix;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public abstract class SpiralMatrixTest {
     SpiralMatrix spiralMatrix;
+
+    @Test
+    void testExample1() {
+        assertEquals(Arrays.asList(1, 2, 3, 6, 9, 8, 7, 4, 5), spiralMatrix.spiralOrder(new int[][]{
+                new int[]{1, 2, 3},
+                new int[]{4, 5, 6},
+                new int[]{7, 8, 9},
+        }));
+    }
+
+    @Test
+    void testExample2() {
+        assertEquals(Arrays.asList(1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7), spiralMatrix.spiralOrder(new int[][]{
+                new int[]{1, 2, 3, 4},
+                new int[]{5, 6, 7, 8},
+                new int[]{9, 10, 11, 12},
+        }));
+    }
 }
